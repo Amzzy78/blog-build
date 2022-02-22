a47f12eaf3e68c31149373b64cfd23579c93fca2
Amzzy78 <murphy.annmarie78@gmail.com>
2022-02-22 16:50:23 +0000

    Add summernote field to PostAdmin class

--- mysite/settings.py
+++ mysite/settings.py
@@ -147,3 +147,4 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+X_FRAME_OPTIONS = 'SAMEORIGIN'
