dedca280455bd4aa9edab00eb265cc0aff786114
Amzzy78 <murphy.annmarie78@gmail.com>
2022-02-23 14:51:28 +0000

    Troubleshoot summernote code issue

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     # path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
-    path('summernote/', include('django_summernote.urls')),
+    
 ]
