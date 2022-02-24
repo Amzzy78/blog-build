de4313b1cd1c21b34d9888925dc5bd217036cf25
Amzzy78 <murphy.annmarie78@gmail.com>
2022-02-24 17:27:17 +0000

    Corrected code

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
-from django.conf.urls.static import static
 from django.urls import path, include
-from django.conf import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('blog.urls'), name='blog_urls'),
-    
-    
+     
 ]
 
 # if settings.DEBUG:
