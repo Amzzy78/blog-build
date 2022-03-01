67784f003270a031b68ed20465a9e55b058a9f7f
Amzzy78 <murphy.annmarie78@gmail.com>
2022-03-01 21:29:24 +0000

    Error loading cloudinary masthead image

--- mysite/settings.py
+++ mysite/settings.py
@@ -87,9 +87,8 @@ JAZZMIN_SETTINGS = {
     "site_header": "FÁILTE GO DTÍ AN BIA SIMPLÍ",
     "site_brand": "Bia Simplí",
     "site_icon": "images/favicon.png",
-    # Add your own branding here
     "bia_simplí": None,
-    "welcome_sign": "FÁILTE GO DTÍ AN BIA SIMPLÍ",
+    "welcome_sign": "Welcome to Bia Simplí",
     # Copyright on the footer
     "copyright": "Bia Simplí",
     "user_avatar": None,
